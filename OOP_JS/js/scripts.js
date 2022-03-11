9ac64103747c6a8b5d1b8c48eac32e1f35e8f717
Paulo Henrique Barros Diniz <83252773+paulodiniz019283@users.noreply.github.com>
2022-03-10 22:56:47 -0300

    Add files via upload

---
+++ OOP_JS/js/scripts.js
@@ -0,0 +1,19 @@
+class Produto {
+
+    constructor() {
+        this.id = 0
+        this.nomeProduto = ''
+        this.valor = 0
+    }
+
+    adicionar() {
+        alert('Vamos adicionar um produto')
+    }
+
+    excluir() {
+        alert('Vamos excluir um novo produto')
+    }
+
+}
+
+var produtor = new Produto();
